8fd281792a00ddce4a2fd922b6d6024c7d794bdb
Kovacs Sandor <sada93@citromail.hu>
2020-12-18 13:13:16 +0100

    1.6 send messega function added

--- js/app.js
+++ js/app.js
@@ -4,6 +4,34 @@ $(document).ready(function(){
         $(".sidebar__members").css({"border-left":"0","padding-left": "15px"})
         $(this).closest(".selected").css({"border-left": "5px solid #6200EA", "padding-left": "10px"})
     });
+    
+    var currentTime = new Date(); 
+    var timeStamp = currentTime.getHours() + ":"  //current time//
+                    + currentTime.getMinutes()
+
+
+        $(".send-button").click(function () {
+            let sendMessage = $("#message").val();
+            let typingIndicator="<h6 class="+"typing-indicator"+">Amy is typing...</h6>"
+            $(".content__chat__wrapper").append('<h6 class="content__chat__metadata--student">'+timeStamp+'</h6>'
+                +'<div class="clearfix message"> <p class="content__chat__message--student short-message layout__item--floated--right">'
+                + sendMessage + '</p>' 
+                +'<img class="content__chat__image--student"' + 'src="assets/user_avatar_07.png" alt="amy">'+'</div>')
+                
+                setTimeout(function(){
+                    $(".content__chat__wrapper").append(typingIndicator)
+                },800);
+
+                setTimeout(function(){
+                    $(".typing-indicator").css("display","none")
+                    $(".content__chat__wrapper").append('<h6 class="content__chat__metadata--amy">Amy, '+timeStamp+'</h6>'
+                     + '<div class="clearfix">' + ' <img class="content__chat__image--amy" src="assets/user_avatar_01.png" alt="amy">'
+                      + '<p class="content__chat__message--amy"> "Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
+                      +' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua erat.</p>' + ' </div>' )
+                },2000);
+                
+                $("#message").val("")
+        });
     });
 
 
