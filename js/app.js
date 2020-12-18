b76b06cf3a359cbb12c4156688cdbb98e99dc379
Kovacs Sandor <sada93@citromail.hu>
2020-12-18 13:25:11 +0100

    1.7 scroll top added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+let myDiv = $(".content__chat__wrapper");
+myDiv.animate({ scrollTop: myDiv.prop("scrollHeight") - 1 }, 300);
+
 $(document).ready(function(){
     $(".sidebar__members").click(function(){
         $(".sidebar__members").addClass("selected")
@@ -17,17 +20,21 @@ $(document).ready(function(){
                 +'<div class="clearfix message"> <p class="content__chat__message--student short-message layout__item--floated--right">'
                 + sendMessage + '</p>' 
                 +'<img class="content__chat__image--student"' + 'src="assets/user_avatar_07.png" alt="amy">'+'</div>')
-                
+                myDiv.animate({ scrollTop: myDiv.prop("scrollHeight") - 1 }, 300);
+
                 setTimeout(function(){
                     $(".content__chat__wrapper").append(typingIndicator)
+                    myDiv.animate({ scrollTop: myDiv.prop("scrollHeight") - 1 }, 300);
                 },800);
+
 
                 setTimeout(function(){
                     $(".typing-indicator").css("display","none")
                     $(".content__chat__wrapper").append('<h6 class="content__chat__metadata--amy">Amy, '+timeStamp+'</h6>'
-                     + '<div class="clearfix">' + ' <img class="content__chat__image--amy" src="assets/user_avatar_01.png" alt="amy">'
-                      + '<p class="content__chat__message--amy"> "Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
-                      +' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua erat.</p>' + ' </div>' )
+                    + '<div class="clearfix">' + ' <img class="content__chat__image--amy" src="assets/user_avatar_01.png" alt="amy">'
+                    + '<p class="content__chat__message--amy"> "Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
+                    +' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua erat.</p>' + ' </div>' )
+                    myDiv.animate({ scrollTop: myDiv.prop("scrollHeight") - 1 }, 300);
                 },2000);
                 
                 $("#message").val("")
